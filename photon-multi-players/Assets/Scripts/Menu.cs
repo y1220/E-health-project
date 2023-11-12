b4e87808b8a5b4e88603f5a4c1d9fff2f0de6b49
y1220 <ga.2.y1220@gmail.com>
2023-11-12 18:57:24 +0100

    feat(Unity): add connection success version of photon project

--- photon-multi-players/Assets/Scripts/Menu.cs
+++ photon-multi-players/Assets/Scripts/Menu.cs
@@ -28,7 +28,7 @@ public class Menu : MonoBehaviourPunCallbacks
     public GameObject roomButtonPrefabs;
 
 
-    private List<GameObject> roomBottons = new List<GameObject>();
+    private List<GameObject> roomButtons = new List<GameObject>();
     private List<RoomInfo> roomList = new List<RoomInfo>();
 
 
@@ -61,6 +61,11 @@ public class Menu : MonoBehaviourPunCallbacks
 
         // activate the requested screen
         screen.SetActive(true);
+
+        if(screen == lobbyBrowserScreen)
+        {
+            UpdateLobbyBrowserUI();
+        }
     }
 
     public void OnBackToMainScreen()
@@ -116,7 +121,10 @@ public class Menu : MonoBehaviourPunCallbacks
 
         // loop through all the players
         foreach (Player player in PhotonNetwork.PlayerList)
-            playerListText.text += player.NickName = "\n";
+        {
+            Debug.Log(player.NickName);
+            playerListText.text += player.NickName + "\n";
+        }
 
         // set the room info text
         roomInfoText.text = "<b>Room Name </b> \n" + PhotonNetwork.CurrentRoom.Name;
@@ -136,5 +144,57 @@ public class Menu : MonoBehaviourPunCallbacks
     {
         PhotonNetwork.LeaveRoom();
         SetScreen(mainScreen);
+    }
+
+    GameObject CreateRoomButton()
+    {
+        GameObject buttonObject = Instantiate(roomButtonPrefabs, roomListContainer.transform);
+        roomButtons.Add(buttonObject);
+        return buttonObject;
+    }
+
+    void UpdateLobbyBrowserUI()
+    {
+        // disable all rooms Buttons
+        foreach(GameObject button in roomButtons)
+        {
+            button.SetActive(false);
+        }
+
+        // display all current rooms in the master client
+        for(int x=0; x<roomList.Count; x++)
+        {
+            // get or create the button object
+            GameObject button = x >= roomButtons.Count ? CreateRoomButton() : roomButtons[x];
+
+            button.SetActive(true);
+            // set the room name and player count text
+            button.transform.Find("Room name Text").GetComponent<TextMeshProUGUI>().text = roomList[x].Name;
+            button.transform.Find("Player Counter Text").GetComponent<TextMeshProUGUI>().text = roomList[x].PlayerCount + " / " + roomList[x].MaxPlayers;
+
+            // set the button when we click on them
+            Button buttoncomp = button.GetComponent<Button>();
+            string roomName = roomList[x].Name;
+            Debug.Log(roomName);
+
+
+            buttoncomp.onClick.RemoveAllListeners();
+            buttoncomp.onClick.AddListener(() => { OnJoinRoomButton(roomName); });
+        }
+    }
+
+    public void OnRefreshButton()
+    {
+        UpdateLobbyBrowserUI();
+    }
+
+    public void OnJoinRoomButton(string roomName)
+    {
+        NetworkManager.instance.JoinRoom(roomName);
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> allRooms)
+    {
+        roomList = allRooms;
     }
 }
