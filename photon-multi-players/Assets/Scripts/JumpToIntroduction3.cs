c0da61bcfa792e2ea330c4d03d5875394666336f
y1220 <ga.2.y1220@gmail.com>
2023-11-17 16:55:40 +0100

    feat(Unity): add introduction scenes

---
+++ photon-multi-players/Assets/Scripts/JumpToIntroduction3.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class JumpToIntroduction3 : MonoBehaviour
+{
+    public void StartBtn()
+    {
+        SceneManager.LoadScene("Introduction-3");
+    }
+}
