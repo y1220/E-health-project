07023900921733e45ba63655590d73a203c05b68
y1220 <ga.2.y1220@gmail.com>
2023-12-03 14:08:21 +0100

    feat(FelmeDoors): add routing of felme doors

---
+++ photon-multi-players/Assets/Scripts/JumpToHeldaDoor2.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+
+public class JumpToHeldaDoor2: MonoBehaviour
+{
+    public void StartBtn()
+    {
+        //NetworkManager.instance.photonView.RPC("ChangeScene", RpcTarget.All, "Thanks");
+        SceneManager.LoadScene("HeldaDoor1");
+    }
+}
