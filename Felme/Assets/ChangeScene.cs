e55154950c987aef57e8380ad58fdbe7bc49b21c
virginia.anton <virginia.anton@mail.polimi.it>
2023-12-17 19:02:33 +0100

    game three

---
+++ Felme/Assets/ChangeScene.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+
+     
+{
+    public void Change(string nameScene)
+    {
+        SceneManager.LoadScene(nameScene);
+    }
+}
